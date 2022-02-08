596159af451652e330328bf0a9fba174b145f3e7
Jozef Bátrna <batrna.jozef@gmail.com>
2022-02-08 14:30:42 +0100

    init project files with added launchpad

---
+++ Source/RunningAround/RunningAroundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RunningAroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARunningAroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARunningAroundGameMode();
+};
+
+
+
