2508d50a2141f28df9ea2611c2a21c6a363ad312
Jozef Bátrna <batrna.jozef@gmail.com>
2022-02-08 18:14:07 +0100

    added endless path generation

--- Source/RunningAround/RunwayGeneratingActor.cpp
+++ Source/RunningAround/RunwayGeneratingActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "RunwayGeneratingActor.h"
+#include "Engine/StaticMeshActor.h"
 
 // Sets default values
 ARunwayGeneratingActor::ARunwayGeneratingActor()
@@ -11,11 +12,29 @@ ARunwayGeneratingActor::ARunwayGeneratingActor()
 
 }
 
+// can't be done in constructor, needs to be in BeginPlay because before the BasicTile isn't initialized
+void ARunwayGeneratingActor::Initialize()
+{
+	if (!BasicTile)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Didn't set Basic Tile"));
+		return;
+	}
+
+	FBox TileBounds = BasicTile->GetComponentsBoundingBox();
+	TileSize = TileBounds.Max - TileBounds.Min;
+	PathVanishDistance = TileSize.X * 3;
+	//UE_LOG(LogTemp, Warning, TEXT("TileSize: %f, %f, %f"), TileSize.X, TileSize.Y, TileSize.Z);
+}
+
 // Called when the game starts or when spawned
 void ARunwayGeneratingActor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	Initialize();
 	
+	//SpawnTile(BasicTile);
 }
 
 // Called every frame
@@ -23,5 +42,57 @@ void ARunwayGeneratingActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	GeneratePath();
+}
+
+void ARunwayGeneratingActor::GeneratePath()
+{
+	if (!PlayerCharacter)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Didn't set Player Character"));
+		return;
+	}
+
+	//UE_LOG(LogTemp, Warning, TEXT("AAAAAAAAAAAAAAAA"));
+
+	// check if path was already generated (for the first time)
+	if (Path.IsEmpty())
+	{
+		for (int i = 0; i < QueueLimit; i++)
+		{
+			CurrentSpawnLocation += FVector(TileSize.X, 0.0f, 0.0f);
+			SpawnTile(BasicTile);
+			UE_LOG(LogTemp, Warning, TEXT("TileSize: %f, %f, %f"), CurrentSpawnLocation.X, CurrentSpawnLocation.Y, CurrentSpawnLocation.Z);
+		}
+		return;
+	}
+	
+	// check if Player's distance from the furthest Tile in the back is greater than default value
+	AActor* LastTile = *Path.Peek();
+	float LastTileDistance = FVector::Dist(LastTile->GetActorLocation(), PlayerCharacter->GetActorLocation());
+	if (LastTileDistance > PathVanishDistance)
+	{
+		for (int i = 0; i < PathVanishDistance; i++)
+		{
+			AActor* VanishedTile;
+			Path.Dequeue(VanishedTile); //TODO just pop? but maybe I could reuse it
+			CurrentSpawnLocation += FVector(TileSize.X, 0.0f, 0.0f);
+			SpawnTile(BasicTile);
+		}
+	}
+
+}
+
+void ARunwayGeneratingActor::SpawnTile(AActor* Tile)
+{
+	const FVector SpawnLocation = CurrentSpawnLocation;
+	const FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;
+
+	UClass* TileClass = Tile->GetClass();
+
+	GetWorld()->SpawnActor(TileClass, &SpawnLocation, &SpawnRotation, SpawnParams);
 }
 
--- Source/RunningAround/RunwayGeneratingActor.h
+++ Source/RunningAround/RunwayGeneratingActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Containers/Queue.h"
 #include "RunwayGeneratingActor.generated.h"
 
 UCLASS()
@@ -18,6 +19,21 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void Initialize();
+	void SpawnTile(AActor* Tile);
+	void GeneratePath();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="SpawnableTiles")
+	AActor* BasicTile;
+
+	UPROPERTY(EditAnywhere)
+	AActor* PlayerCharacter;
+		
+	FVector TileSize; // dimensions
+	float PathVanishDistance; // distance where tiles should be removed from the back
+	FVector CurrentSpawnLocation = FVector(-39.0f, -47.0f, 3.0f); // initialized to the starting location
+	int QueueLimit = 10; // max number of tiles in the queue
+	TQueue<AActor*> Path;
 
 public:	
 	// Called every frame
